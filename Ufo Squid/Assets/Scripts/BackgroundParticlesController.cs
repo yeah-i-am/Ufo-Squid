c1bcd3603fdc59c8d1eb3aeb6624b45d831270e7
Eugene <peugene2406@gmail.com>
2021-04-01 12:21:41 +0300

    Edited background

---
+++ Ufo Squid/Assets/Scripts/BackgroundParticlesController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParticlesController : MonoBehaviour
+{
+    public ParticleSystem[] backgroundParticles;
+
+    private ParticleSystem.Particle[] particles = new ParticleSystem.Particle[500];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (ParticleSystem ps in backgroundParticles)
+        {
+            /* Changing particle system scale */
+            float scale = 11f * Screen.width / Screen.height;
+            var shape = ps.shape;
+            shape.scale = new Vector3(scale, 11f, 1f);
+
+            /* Changing particles size */
+            var main = ps.main;
+            main.startSize = 300f / Screen.dpi;
+
+            ps.Emit(500);
+            ps.GetParticles(particles);
+
+            for(int i = 0; i < particles.Length; i++)
+                particles[i].remainingLifetime = Random.Range(0, particles[i].startLifetime);
+
+            ps.SetParticles(particles);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
